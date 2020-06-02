8a5a6068da14c2b325f52201e5d99873386651d1
liuzhengbang <liuzhengbang@gmail.com>
2020-06-02 23:20:02 +0800

    update stock data query

---
+++ analysis.py
@@ -0,0 +1,11 @@
+# from linearRegression import linear
+from stockQuery.individualStock import query_day_k_data
+from stockQuery.stockBasic import *
+
+login()
+query_day_k_data("sh.000935")
+logout()
+
+
+# linear
+
---
+++ stockQuery/individualStock.py
@@ -0,0 +1,24 @@
+import baostock as bs
+import pandas as pd
+import time
+
+
+def query_day_k_data(code):
+
+    rs = bs.query_history_k_data_plus(code,
+                                      "date,code,open,high,low,close,volume,amount,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM",
+                                      start_date='2010-01-01',
+                                      frequency="d",
+                                      adjustflag="3")
+    if rs.error_code != '0':
+        print('query_history_k_data_plus respond error_code:' + rs.error_code)
+        print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
+        return
+
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        data_list.append(rs.get_row_data())
+    result = pd.DataFrame(data_list, columns=rs.fields)
+
+    result.to_csv(code+"_day_k_data.csv", index=False)
+    print(result)
