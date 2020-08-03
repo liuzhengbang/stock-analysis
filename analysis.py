3561c3e6f94a1d013ac338e660b35bfc6525dbc4
liuzhengbang <liuzhengbang@gmail.com>
2020-08-03 23:53:27 +0800

    add rolling data

--- analysis.py
+++ analysis.py
@@ -2,7 +2,8 @@ import pandas
 import torch
 
 from csv_utils import read_individual_csv, read_index_csv, get_all_stocks_code
-from data_pool.analysis_data import construct_y, construct_x, construct_dataset_with_index, DataException
+from data_pool.analysis_data import construct_y, construct_x, construct_dataset_with_index, DataException, \
+    construct_dataset_with_index_and_history
 from train.trainer import train_model
 from torch.utils.data.dataset import Dataset
 
@@ -43,7 +44,7 @@ def filter_list(stock_list):
     for stock in stock_list[::-1]:
         # print("processing", stock)
         try:
-            construct_dataset_with_index(stock, index_list_analysis, filtering_only=True)
+            construct_dataset_with_index_and_history(stock, index_list_analysis, filtering_only=True)
         except DataException:
             print("remove", stock, "from list")
             stock_list.remove(stock)
@@ -51,7 +52,7 @@ def filter_list(stock_list):
 
 
 all_stock_list = get_all_stocks_code()
-prepare_data(all_stock_list, index_list_query)
+prepare_data(all_stock_list, index_list_query, append=False)
 # all_stock_list = ["sz.002120", "sh.600669", "sh.600670"]
 filter_list(all_stock_list)
 
@@ -61,17 +62,15 @@ class TrainingDataset(Dataset):
         self.stock_list = stock_list
 
     def __getitem__(self, index):
-        # print("dataset get stock", self.stock_list[index])
-        x, y = construct_dataset_with_index(self.stock_list[index], index_list_analysis)
+        x, y = construct_dataset_with_index_and_history(self.stock_list[index], index_list_analysis)
         return x, y
 
     def __len__(self):
-        # print("dataset length", len(self.stock_list))
         return len(self.stock_list)
 
 
 # x_train, y_train = construct_dataset_with_index(all_stock_list[0], index_list_analysis)
-x_test, y_test = construct_dataset_with_index("sz.002120", index_list_analysis)
+x_test, y_test = construct_dataset_with_index_and_history("sz.002120", index_list_analysis)
 # for code in all_stock_list[1:]:
 #
 #     try:
@@ -84,7 +83,6 @@ x_test, y_test = construct_dataset_with_index("sz.002120", index_list_analysis)
 #         y_train = torch.cat([y_train, y_temp], dim=0)
 
 
-# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 dataset = TrainingDataset(all_stock_list)
 loader = torch.utils.data.DataLoader(dataset, batch_size=100, num_workers=0, shuffle=False)
 
--- data_pool/analysis_data.py
+++ data_pool/analysis_data.py
@@ -6,6 +6,7 @@ from csv_utils import read_individual_csv, read_index_csv
 individual_cols_sel = ['open', 'close', 'amount', 'high', 'low', 'volume', 'peTTM', 'pbMRQ']
 index_cols_sel = ['open', 'high', 'low', 'close', 'volume', 'amount']
 device = torch.device('cuda:0')
+rolling_days = [2, 3, 4, 5, 6, 7, 14, 30, 60, 100, 300, 600, 1000]
 
 
 class DataException(Exception):
@@ -23,8 +24,9 @@ def construct_x(code):
     return dataset_x
 
 
-def construct_dataset_with_index(code, index_code_list, shift=1, filtering_only=False):
+def construct_dataset_with_index(code, index_code_list, filtering_only=False):
     csv_data = read_individual_csv(code)
+    csv_data.drop(labels=0, inplace=True)
     title_list = individual_cols_sel.copy()
 
     for index_code in index_code_list:
@@ -36,13 +38,61 @@ def construct_dataset_with_index(code, index_code_list, shift=1, filtering_only=
     dataset_x = pd.DataFrame(csv_data, columns=title_list)
     dataset_y = pd.DataFrame(csv_data, columns=['pctChg'])
 
-    if len(dataset_x) < shift:
+    if len(dataset_x) < 1:
         raise DataException(code)
 
     if filtering_only:
         return
 
-    dataset_x.drop(labels=len(dataset_x) - shift, inplace=True)
+    dataset_x.drop(labels=len(dataset_x) - 1, inplace=True)
+    dataset_y.drop(labels=0, inplace=True)
+
+    dataset_x = torch.tensor(dataset_x.values).to(device).float()
+    dataset_y = torch.tensor(dataset_y.values).to(device).float()
+
+    convertPctChgToBool(dataset_y)
+    return dataset_x, dataset_y
+
+
+def add_rolling_data(csv_data, title_list):
+    for days in rolling_days:
+        csv_data['pctChg_' + str(days)] = csv_data['pctChg'].rolling(days).sum()
+        title_list.append('pctChg_' + str(days))
+        csv_data['volume_' + str(days)] = csv_data['volume'].rolling(days).sum()
+        title_list.append('volume_' + str(days))
+
+
+def construct_dataset_with_index_and_history(code, index_code_list, filtering_only=False):
+    history_length = 1000
+    csv_data = read_individual_csv(code)
+    if len(csv_data) <= 1000:
+        raise DataException(code)
+
+    csv_data.drop(labels=0, inplace=True)
+
+    title_list = individual_cols_sel.copy()
+
+    for index_code in index_code_list:
+        index_data = read_index_csv(index_code)
+        csv_data = pd.merge(csv_data, index_data, how="inner", on="date", suffixes=('', '_' + index_code))
+        for sel in index_cols_sel:
+            title_list.append(sel + '_' + index_code)
+
+    if len(csv_data) <= 1000:
+        raise DataException(code)
+
+    add_rolling_data(csv_data, title_list)
+
+    csv_data.drop(labels=range(0, history_length - 1), axis=0, inplace=True)
+    csv_data = csv_data.reset_index(drop=True)
+
+    dataset_x = pd.DataFrame(csv_data, columns=title_list)
+    dataset_y = pd.DataFrame(csv_data, columns=['pctChg'])
+
+    if filtering_only:
+        return
+
+    dataset_x.drop(labels=len(dataset_x) - 1, inplace=True)
     dataset_y.drop(labels=0, inplace=True)
 
     dataset_x = torch.tensor(dataset_x.values).to(device).float()
@@ -66,5 +116,3 @@ def convertPctChgToBool(pct_chg):
             pct_chg[value] = 1.
         else:
             pct_chg[value] = 0.
-
-
